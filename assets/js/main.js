07745727ca9d8a020c5a0830ee3ff9c178150efd
HusanboyZafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2023-05-03 10:17:24 +0500

    adding meta tags and changing navigation bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,84 @@
-var span = document.getElementById('time');
-function time() {
-    var d = new Date();
-    var seconds = d.getSeconds();
-    var minuts = d.getMinutes();
-    var hours = d.getHours();
-    var days = d.getDay();
-    var months = d.getMonth();
-    var year = d.getFullYear();
-    if (seconds < 10) {
-        seconds = `0${seconds}`
+header_navigation_item = document.querySelectorAll(".header_navigation_item")
+let closer = document.querySelector(".closer")
+
+header_navigation_item.forEach((element) => {
+    element.addEventListener("click", () => {
+        inner_el = element.lastElementChild
+        closer.classList.add("active")
+        inner_el_first = element.firstElementChild
+        let header_navigation_links_wrapper = document.querySelectorAll(".header_navigation_links_wrapper")
+
+        if (inner_el.getBoundingClientRect().height) {
+            inner_el.style.maxHeight = null
+            inner_el.style.maxHeight = `0px`
+            let header_navigation_item_span = document.querySelectorAll(".header_navigation_item span")
+            header_navigation_item_span.forEach(element => {
+                element.classList.remove("active")
+            });
+        }
+        else {
+            header_navigation_links_wrapper.forEach((element) => {
+                element.style.maxHeight = 0
+            });
+            let header_navigation_item_span = document.querySelectorAll(".header_navigation_item span")
+            header_navigation_item_span.forEach(element => {
+                element.classList.remove("active")
+            });
+            inner_el.style.display = "flex"
+            inner_el.style.maxHeight = inner_el.scrollHeight + "px"
+            let header_navigation_list = document.querySelector(".header_navigation_list")
+            header_navigation_list.style.maxHeight = header_navigation_list.scrollHeight + 400 + "px"
+            inner_el_first.classList.add("active")
+        }
+    })
+});
+
+let header_burger = document.querySelector(".header_burger")
+
+header_burger.addEventListener('click', () => {
+    header_burger.classList.toggle("active")
+    let header_navigation_list = document.querySelector(".header_navigation_list")
+
+    if (header_navigation_list.getBoundingClientRect().height) {
+        header_navigation_list.style.maxHeight = null
+        header_navigation_list.style.maxHeight = `0px`
     }
-    if (minuts < 10) {
-        minuts = `0${minuts}`
+    else {
+        header_navigation_list.style.display = "flex"
+        header_navigation_list.style.maxHeight = header_navigation_list.scrollHeight + "px"
     }
-    if (hours < 10) {
-        hours = `0${hours}`
-    }
-    if (days < 10) {
-        days = `0${days}`
-    }
-    if (months < 10) {
-        months = `0${months}`
-    }
-    span.innerHTML = `${hours}:${minuts}:${seconds} (GMT+5) | ${days}.${months}.${year}`
-}
-setInterval(time, 1000);
+
+    header_navigation_item.forEach((element) => {
+        inner_el = element.lastElementChild
+        let header_navigation_links_wrapper = document.querySelectorAll(".header_navigation_links_wrapper")
+        if (inner_el.getBoundingClientRect().height) {
+            inner_el.style.maxHeight = null
+            inner_el.style.maxHeight = `0px`
+        }
+        else {
+            header_navigation_links_wrapper.forEach((element) => {
+                element.style.maxHeight = 0
+            });
+            inner_el.style.display = "flex"
+            inner_el.style.maxHeight = inner_el.scrollHeight + "px"
+        }
+    });
+    closer.classList.remove("active")
+})
+
+
+closer.addEventListener("click", () => {
+    header_navigation_item.forEach((element) => {
+        inner_el = element.lastElementChild
+        inner_el_first = element.firstElementChild
+        let header_navigation_links_wrapper = document.querySelectorAll(".header_navigation_links_wrapper")
+
+        inner_el.style.maxHeight = null
+        inner_el.style.maxHeight = `0px`
+        let header_navigation_item_span = document.querySelectorAll(".header_navigation_item span")
+        header_navigation_item_span.forEach(element => {
+            element.classList.remove("active")
+        });
+    });
+    closer.classList.remove("active")
+})
