ad442d4ea544389775fb3a59f6c3512c9221f172
HusanboyZafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2023-05-03 13:19:19 +0500

    adding swiper and news to main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,43 @@
 header_navigation_item = document.querySelectorAll(".header_navigation_item")
+let hero_action_item = document.querySelectorAll(".main-hero_action_item")
+
 let closer = document.querySelector(".closer")
+hero_action_item.forEach((element) => {
+    element.addEventListener("click", () => {
+        inner_el = element.lastElementChild
+        closer.classList.add("active")
+        inner_el_first = element.firstElementChild
+        console.log(inner_el_first);
+
+        let hero_action_posts = document.querySelectorAll(".main-hero_action_posts")
+        inner_el_first = element.firstElementChild
+        if (inner_el.getBoundingClientRect().height) {
+            inner_el.style.maxHeight = null
+            inner_el.style.maxHeight = `0px`
+            inner_el.style.marginTop = `0px`
+            let hero_action_item = document.querySelectorAll(".main-hero_action_item span")
+            hero_action_item.forEach(element => {
+                element.classList.remove("active")
+            });
+            closer.classList.remove("active")
+        }
+        else {
+            hero_action_posts.forEach((element) => {
+                element.style.maxHeight = 0
+            });
+            let hero_action_item = document.querySelectorAll(".main-hero_action_item span")
+            hero_action_item.forEach(element => {
+                element.classList.remove("active")
+            });
+            setTimeout(() => {
+                inner_el.style.display = "flex"
+                inner_el.style.maxHeight = inner_el.scrollHeight + "px"
+                inner_el_first.classList.add("active")
+            }, 300)
+        }
+    })
+});
+
 
 header_navigation_item.forEach((element) => {
     element.addEventListener("click", () => {
@@ -7,10 +45,13 @@ header_navigation_item.forEach((element) => {
         closer.classList.add("active")
         inner_el_first = element.firstElementChild
         let header_navigation_links_wrapper = document.querySelectorAll(".header_navigation_links_wrapper")
-
+        header_navigation_links_wrapper.forEach(el => {
+            el.style.marginTop = "0px"
+        })
         if (inner_el.getBoundingClientRect().height) {
             inner_el.style.maxHeight = null
             inner_el.style.maxHeight = `0px`
+            inner_el.style.marginTop = `0px`
             let header_navigation_item_span = document.querySelectorAll(".header_navigation_item span")
             header_navigation_item_span.forEach(element => {
                 element.classList.remove("active")
@@ -30,8 +71,9 @@ header_navigation_item.forEach((element) => {
                 inner_el.style.maxHeight = inner_el.scrollHeight + "px"
                 let header_navigation_list = document.querySelector(".header_navigation_list")
                 header_navigation_list.style.maxHeight = header_navigation_list.scrollHeight + 400 + "px"
+                inner_el.style.marginTop = `20px`
                 inner_el_first.classList.add("active")
-            }, 500)
+            }, 300)
         }
     })
 });
@@ -70,12 +112,26 @@ closer.addEventListener("click", () => {
         inner_el = element.lastElementChild
         inner_el_first = element.firstElementChild
 
-        let header_navigation_links_wrapper = document.querySelectorAll(".header_navigation_links_wrapper")
+        inner_el.style.maxHeight = null
+        inner_el.style.maxHeight = `0px`
+        inner_el.style.marginTop = `0px`
+        inner_el_first.classList.remove("active")
+        let header_navigation_item_span = document.querySelectorAll(".header_navigation_item span")
+        header_navigation_item_span.forEach(element => {
+            element.classList.remove("active")
+        });
+    });
+
+    hero_action_item.forEach((element) => {
+        inner_el = element.lastElementChild
+        inner_el_first = element.firstElementChild
 
         inner_el.style.maxHeight = null
         inner_el.style.maxHeight = `0px`
-        let header_navigation_item_span = document.querySelectorAll(".header_navigation_item span")
-        header_navigation_item_span.forEach(element => {
+        inner_el.style.marginTop = `0px`
+        inner_el_first.classList.remove("active")
+        let hero_action_posts = document.querySelectorAll(".main-hero_action_posts span")
+        hero_action_posts.forEach(element => {
             element.classList.remove("active")
         });
     });
@@ -83,7 +139,9 @@ closer.addEventListener("click", () => {
 })
 
 let swiper_slider = document.querySelector(".swiper")
-let hero_head_wrapper = document.querySelectorAll(".main-hero_head_wrapper")
+let swiper_second = document.querySelector(".swiper_second")
+let hero_head_wrapper = document.querySelectorAll(".swiper .main-hero_head_wrapper")
+let hero_head_wrapper_news = document.querySelectorAll(".swiper_second .main-hero_head_wrapper")
 
 
 window.addEventListener("load", () => {
@@ -115,3 +173,30 @@ window.addEventListener("resize", () => {
 })
 
 
+window.addEventListener("load", () => {
+    nums = [0, 200, 500]
+    hero_head_wrapper_news.forEach((el) => {
+        nums.push(el.getBoundingClientRect().height)
+    })
+    max = Math.max.apply(null, nums)
+    swiper_second.style.height = `${max}px`
+})
+
+window.addEventListener("scroll", () => {
+    nums = [0, 200, 500]
+    hero_head_wrapper_news.forEach((el) => {
+        nums.push(el.getBoundingClientRect().height)
+    })
+    max = Math.max.apply(null, nums)
+    swiper_second.style.height = `${max}px`
+})
+
+
+window.addEventListener("resize", () => {
+    nums = [0, 200, 500]
+    hero_head_wrapper_news.forEach((el) => {
+        nums.push(el.getBoundingClientRect().height)
+    })
+    max = Math.max.apply(null, nums)
+    swiper_second.style.height = `${max}px`
+})
--- assets/lib/js/swiper_settings.js
+++ assets/lib/js/swiper_settings.js
@@ -12,3 +12,18 @@ const swiper = new Swiper('.swiper', {
         delay: 2000,
     },
 });
+
+const swiper_sec = new Swiper('.swiper_second', {
+    direction: 'horizontal',
+    loop: true,
+    grabCursor: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    speed: 1000,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 2000,
+    },
+});
